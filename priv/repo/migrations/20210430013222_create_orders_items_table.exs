515efb5ddbda11c50db123a4da9096a2ea0d0cd2
LuizFerK <luizfernandoklein@live.com>
2021-05-12 20:24:49 -0300

    fix: Delete order relations with on_delete option

--- priv/repo/migrations/20210430013222_create_orders_items_table.exs
+++ priv/repo/migrations/20210430013222_create_orders_items_table.exs
@@ -3,8 +3,8 @@ defmodule Rockelivery.Repo.Migrations.CreateOrdersItemsTable do
 
   def change do
     create table :orders_items, primary_key: false do
-      add :order_id, references(:orders, type: :binary_id)
-      add :item_id, references(:items, type: :binary_id)
+      add :order_id, references(:orders, type: :binary_id, on_delete: :delete_all)
+      add :item_id, references(:items, type: :binary_id, on_delete: :delete_all)
     end
   end
 end
